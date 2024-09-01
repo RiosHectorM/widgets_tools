a77f54e13d17325b44a589dfb1e52ac2378d5a06
RiosHectorM <rios.hector.martin@gmail.com>
2024-09-01 19:39:19 -0300

    screens created, barrel updated

--- lib/config/presentation/screens/screens.dart
+++ lib/config/presentation/screens/screens.dart
@@ -1,5 +1,9 @@
+export 'package:widgets_tools/config/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_tools/config/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_tools/config/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_tools/config/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_tools/config/presentation/screens/home/home_screen.dart';
-
-
+export 'package:widgets_tools/config/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_tools/config/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_tools/config/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_tools/config/presentation/screens/ui_controls/ui_controls_screen.dart';
