69ccea29f07a094949da4ffcf9603674fb6975f3
RiosHectorM <rios.hector.martin@gmail.com>
2024-09-09 22:54:16 -0300

    Tutorial Screen in progress

--- lib/config/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/config/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,10 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_tools/config/presentation/screens/buttons/buttons_screen.dart';
+import 'package:go_router/go_router.dart';
 
-class AppTutorialScreen extends StatelessWidget {
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(
+      {required this.title, required this.caption, required this.imageUrl});
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+      title: 'Busca la comida',
+      caption:
+          'Commodo aute ea est eiusmod ipsum enim nulla nostrud. Aliquip proident cupidatat et commodo cupidatat non minim laborum in anim. Cupidatat eu deserunt aute cupidatat qui adipisicing minim elit non in aute pariatur.',
+      imageUrl: 'assets/images/1.png'),
+  SlideInfo(
+      title: 'Entrega rapida',
+      caption:
+          'Mollit deserunt eiusmod deserunt minim est duis. Commodo proident ut nisi do ipsum cillum laborum nulla sint eu. Irure aliqua minim elit sit laboris minim sint ea proident nisi aliquip.',
+      imageUrl: 'assets/images/2.png'),
+  SlideInfo(
+      title: 'Disfruta la comida',
+      caption:
+          'Incididunt pariatur anim fugiat laborum mollit sunt reprehenderit sunt enim amet. Esse consequat enim duis laborum deserunt proident magna incididunt. Sit quis eiusmod deserunt cillum officia occaecat dolore magna reprehenderit ut nulla aliquip commodo. Ullamco cillum id amet nisi ea elit esse ad. Ipsum voluptate quis culpa dolor non tempor ad laborum amet minim Lorem consequat labore.',
+      imageUrl: 'assets/images/3.png')
+];
+
+class AppTutorialScreen extends StatefulWidget {
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  final PageController pageViewController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map((slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl))
+                .toList(),
+          ),
+          Positioned(
+              right: 20,
+              top: 50,
+              child: TextButton(
+                child: const Text("Skip"),
+                onPressed: () => context.pop(),
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: AssetImage(imageUrl),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              title,
+              style: titleStyle,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              caption,
+              style: captionStyle,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
