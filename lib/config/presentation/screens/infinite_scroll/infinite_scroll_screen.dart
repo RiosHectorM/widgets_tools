cc2da19789563be2a9c1e27c99b602114cb66647
RiosHectorM <rios.hector.martin@gmail.com>
2024-09-11 23:13:11 -0300

    InfiniteScrollScreen in progress

--- lib/config/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/config/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
   const InfiniteScrollScreen({super.key});
+
+  @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  List<int> imagesIds = [1, 2, 3, 4, 5];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(itemBuilder: (context, index) {
+      body: ListView.builder(
+        itemCount: imagesIds.length,
+        itemBuilder: (context, index) {
         return FadeInImage(
             width: double.infinity,
             height: 300,
             fit: BoxFit.cover,
             placeholder: const AssetImage('assets/images/jar-loading.gif'),
-            image: NetworkImage('https://picsum.photos/id/1/500/300'));
+            image: NetworkImage('https://picsum.photos/id/${imagesIds[index]}/500/300'));
       }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()=>context.pop()
+      ),
     );
   }
 }
