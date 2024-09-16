f0bc65e8a391181a75202eb0facff4036ee39c40
RiosHectorM <rios.hector.martin@gmail.com>
2024-09-15 23:17:55 -0300

    DarkMode in proggress

--- lib/config/presentation/screens/counter/counter_screen.dart
+++ lib/config/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_tools/config/presentation/providers/counter_provider.dart';
+import 'package:widgets_tools/config/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   const CounterScreen({super.key});
@@ -8,18 +9,28 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Counter Screen',
             style: Theme.of(context).textTheme.titleLarge),
+            actions: [IconButton(
+              icon: Icon(isDarkMode ? Icons.dark_mode_outlined:Icons.light_mode_outlined),
+              onPressed: (){
+                ref.read(isDarkModeProvider.notifier).state = !isDarkMode;      
+              }, 
+            )],
+
       ),
       body: Center(
         child: Text('Valor: $clickCounter'),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+        },
       ),
     );
   }
